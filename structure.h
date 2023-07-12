7189a95fadd6dc3ea183da674495e215dc9842ce
Danee2108 <daniel.casquino@utec.edu.pe>
2023-07-11 22:10:05 -0500

    Added NN base

---
+++ structure.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <random>
+
+using namespace std;
+
+namespace mondongo {
+    class NeuralNetwork {
+        cv::Mat inputLayer; // Vector containing current image pixels
+        cv::Mat hiddenLayer; // Vector containing the hidden layer values
+        double hiddenNeurons = 0; // Size of hidden layer
+        int resolution; // Size of input layer
+    public:
+        double hiddenLayerSize() { return hiddenNeurons; }
+        NeuralNetwork() {
+            hiddenNeurons = 0;
+        }
+        NeuralNetwork(int hiddenNeurons) {
+            hiddenNeurons = hiddenNeurons;
+            hiddenLayer.reshape(hiddenNeurons);
+        }
+        void train(vector<pair<cv::Mat, int>> &dataset, int timesPerImage) {
+        }
+    };
+};
