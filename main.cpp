627c6f90bfc68bcbaf58a47e3c55f6dccb23bbee
Danee2108 <daniel.casquino@utec.edu.pe>
2023-07-11 20:08:27 -0500

    Menu almost there, NN pending

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
-#include <opencv2/core/utils/logger.hpp>
-
-#include "imageloading.h"
 #include "menu.h"
 
 using namespace std;
 
 int main() {
-    //start();
     cv::utils::logging::setLogLevel(cv::utils::logging::LogLevel::LOG_LEVEL_SILENT);
-    string imagePath = "./mnist/t10k-images.idx3-ubyte";
-    string labelPath = "./mnist/t10k-labels.idx1-ubyte";
+    mondongo::start();/*
+    string imagePath = "./mnist/train-images.idx3-ubyte";
+    string labelPath = "./mnist/train-labels.idx1-ubyte";
     vector<pair<cv::Mat, int>> images;
-    mondongo::loadMnist(imagePath, labelPath, ref(images));
-    for (const auto &e : images) {
-        cout << "Current image label: " << e.second << endl;
-        cout << "Current image values: " << endl;
-        //mondongo::displayImageChars(e.first);
-        mondongo::flatten(e.first);
-        mondongo::showImage(e.first);
-    }
+    mondongo::loadMnist(imagePath, labelPath, ref(images));*/
     return 0;
 }
