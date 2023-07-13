fa156b97ca66cbf3cce0adabf0bb3095f0aecee3
Snah-s <camiloernestosoto@gmail.com>
2023-07-13 17:16:59 -0500

    nueva adaptación de la neural network

--- src/structure.h
+++ src/structure.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include "imageloading.h"
 #include <random>
 #include <cmath>
 
@@ -16,6 +17,8 @@ namespace mondongo {
         cv::Mat outputLayer; // Vector containing the output layer values
         cv::Mat hiddenWeights; // Weights for the connections between input and hidden layers
         cv::Mat outputWeights; // Weights for the connections between hidden and output layers
+        cv::Mat hiddenBias; // Bias for the hidden layer
+        cv::Mat outputBias; // Bias for the output layer
         double hiddenNeurons = 0; // Size of hidden layer
         int inputSize; // Size of input layer
         int outputSize; // Size of output layer
@@ -33,19 +36,21 @@ namespace mondongo {
             return result;
         }
         
-        void initializeWeights() {
+        void initializeWeightsAndBiases() {
             cv::RNG rng; // se crea numeros aleatorios de la libreria opencv
             rng.fill(hiddenWeights, cv::RNG::UNIFORM, cv::Scalar(-1), cv::Scalar(1)); // se llena la matriz de pesos con numeros aleatorios
             rng.fill(outputWeights, cv::RNG::UNIFORM, cv::Scalar(-1), cv::Scalar(1)); // se llena la matriz de pesos con numeros aleatorios
+            rng.fill(hiddenBias, cv::RNG::UNIFORM, cv::Scalar(-1), cv::Scalar(1));
+            rng.fill(outputBias, cv::RNG::UNIFORM, cv::Scalar(-1), cv::Scalar(1));
         }
         
         cv::Mat forwardPropagation(const cv::Mat& input) {
             // Calculate values of the hidden layer
-            cv::Mat hiddenInput = input * hiddenWeights; // se multiplica la matriz de entrada por la matriz de pesos
+            cv::Mat hiddenInput = input * hiddenWeights + hiddenBias; // se multiplica la matriz de entrada por la matriz de pesos y se suma el bias
             hiddenLayer = sigmoid(hiddenInput); // se aplica la funcion de activacion a la matriz resultante
             
             // Calculate values of the output layer
-            cv::Mat outputInput = hiddenLayer * outputWeights; // se multiplica la matriz de entrada por la matriz de pesos
+            cv::Mat outputInput = hiddenLayer * outputWeights + outputBias; // se multiplica la matriz de entrada por la matriz de pesos y se suma el bias
             outputLayer = sigmoid(outputInput); // se aplica la funcion de activacion a la matriz resultante
             
             return outputLayer;
@@ -57,40 +62,43 @@ namespace mondongo {
             // Calculate errors in the output layer
             cv::Mat outputErrors = (target - outputLayer) * outputLayer.mul(1 - outputLayer); // se calcula el error de la capa de salida con la formula (target - outputLayer) * outputLayer * (1 - outputLayer)
             
-            // Update weights between hidden and output layers
+            // Update weights and biases between hidden and output layers
             cv::Mat outputDelta = learningRate * hiddenLayer.t() * outputErrors; // se calcula el delta de la capa de salida con la formula learningRate * hiddenLayer.t() * outputErrors
             outputWeights += outputDelta; // se actualizan los pesos de la capa de salida
+            cv::reduce(outputDelta, outputBias, 0, cv::REDUCE_SUM);
             
             // Calculate errors in the hidden layer
             cv::Mat hiddenErrors = outputErrors * outputWeights.t() * hiddenLayer.mul(1 - hiddenLayer); // se calcula el error de la capa oculta con la formula outputErrors * outputWeights.t() * hiddenLayer * (1 - hiddenLayer)
             
-            // Update weights between input and hidden layers
+            // Update weights and biases between input and hidden layers
             cv::Mat hiddenDelta = learningRate * input.t() * hiddenErrors;  // se calcula el delta de la capa oculta con la formula learningRate * input.t() * hiddenErrors
             hiddenWeights += hiddenDelta; // se actualizan los pesos de la capa oculta
+            cv::reduce(hiddenDelta, hiddenBias, 0, cv::REDUCE_SUM);
         }
     public:
     // NN
         NeuralNetwork() = default;
-        NeuralNetwork(int inputSize, int hiddenNeurons) { // se le pasa el tamaño de la capa de entrada, el tamaño de la capa oculta y el tamaño de la capa de salida
-            const int outputSize = 10; // se asigna el tamaño de la capa de salida
-            this->inputSize = inputSize; // se asigna el tamaño de la capa de entrada
-            this->hiddenNeurons = hiddenNeurons; // se asigna el tamaño de la capa oculta
+        NeuralNetwork(int inputSize, int hiddenNeurons, int outputSize) { // se le pasa el tamaño de la capa de entrada, el tamaño de la capa oculta y el tamaño de la capa de salida
+            this->inputSize = inputSize;
+            this->hiddenNeurons = hiddenNeurons;
+            this->outputSize = outputSize;
             
-            inputLayer = cv::Mat::zeros(inputSize, 1, CV_64F); // se crea una matriz de ceros con el tamaño de la capa de entrada la cual es de 784
-            hiddenLayer = cv::Mat::zeros(hiddenNeurons, 1, CV_64F); // se crea una matriz de ceros con el tamaño de la capa oculta la cual es de 15
-            outputLayer = cv::Mat::zeros(1, CV_64F); // se crea una matriz de ceros con el tamaño de la capa de salida la cual es de 10
+            hiddenWeights = cv::Mat::zeros(inputSize, hiddenNeurons, CV_64F); // se inicializan los pesos de la capa oculta a cero
+            outputWeights = cv::Mat::zeros(hiddenNeurons, outputSize, CV_64F); // se inicializan los pesos de la capa de salida a cero
             
-            hiddenWeights = cv::Mat::zeros(inputSize, hiddenNeurons, CV_64F); // se crea una matriz de ceros con el tamaño de la capa de entrada y la capa oculta
-            outputWeights = cv::Mat::zeros(hiddenNeurons, CV_64F); // se crea una matriz de ceros con el tamaño de la capa oculta y la capa de salida
+            hiddenBias = cv::Mat::zeros(1, hiddenNeurons, CV_64F); // se inicializa el bias de la capa oculta a cero
+            outputBias = cv::Mat::zeros(1, outputSize, CV_64F); // se inicializa el bias de la capa de salida a cero
             
-            initializeWeights(); // se inicializan los pesos
+            initializeWeightsAndBiases(); // se inicializan los pesos y biases con números aleatorios
         }
-        NeuralNetwork(string dataPath) {};
-        ~NeuralNetwork(){};
 
-        void train(string &imagePath, string &labelPath) {
-            vector<pair<cv::Mat, int>> mnist;
-            loadMnist(imagePath, labelPath, ref(mnist));
+        void train(const cv::Mat& input, const cv::Mat& target, double learningRate) {
+            cv::Mat output = forwardPropagation(input);
+            backwardPropagation(input, target, learningRate);
+        }
+
+        cv::Mat predict(const cv::Mat& input) {
+            return forwardPropagation(input);
         }
     };
-};
+}
